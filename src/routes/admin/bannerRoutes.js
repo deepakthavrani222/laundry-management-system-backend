f5634748f1a8fd56b5df9d64961e1f0865f39980
deepakThavrani <deepakthavrani474@gmail.com>
2026-01-07 22:39:05 +0530

    fix: Correct middleware path in banner routes
    - Changed '../../middleware/auth' to '../../middlewares/auth'
    - Fixes deployment error: Cannot find module '../../middleware/auth'
    - Middleware folder is 'middlewares' (plural) not 'middleware'

--- src/routes/admin/bannerRoutes.js
+++ src/routes/admin/bannerRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bannerController = require('../../controllers/admin/bannerController');
-const { protect, authorize } = require('../../middleware/auth');
+const { protect, authorize } = require('../../middlewares/auth');
 const { upload } = require('../../services/imageUploadService');
 
 // Apply authentication and authorization middleware
--- src/routes/superAdmin/bannerRoutes.js
+++ src/routes/superAdmin/bannerRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bannerController = require('../../controllers/superAdmin/bannerController');
-const { protect, authorize } = require('../../middleware/auth');
+const { protect, authorize } = require('../../middlewares/auth');
 
 // Apply authentication and authorization middleware
 router.use(protect);
